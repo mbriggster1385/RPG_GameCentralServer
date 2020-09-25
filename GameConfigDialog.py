ac8715c6e5741770aac671598d601d514b382872
Mike Briggs <mbriggster@gmail.com>
2020-09-24 22:59:43 -0500

    Started defining the game config dialog

--- GameConfigDialog.py
+++ GameConfigDialog.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from tkinter.ttk import *
+from GameClass import *
 
 class GameConfigDialog:
 	def __init__(self, root):
 		self.root = root
+		self.game = GameClass()
 
 	def Create_Toplevel(self):
 		self.root.wm_attributes("-disabled", True)
@@ -12,19 +16,30 @@ class GameConfigDialog:
 		self.root.toplevel_dialog.minsize(600, 300)
 		self.root.toplevel_dialog.title("Configure Game")
 		self.root.toplevel_dialog.transient(self.root)
-		self.root.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.Close_Toplevel)
+		self.root.toplevel_dialog.protocol("WM_DELETE_WINDOW", self.cancel_clicked)
 
+		self.root.toplevel_dialog_label = ttk.Label(self.root.toplevel_dialog, text="\n         New Game Name:   \n")
+		self.root.toplevel_dialog_label.grid(column=0, row=1)
+		self.root.toplevel_dialog_entry = ttk.Entry(self.root.toplevel_dialog,width=60)
+		self.root.toplevel_dialog_entry.grid(column=1, row=1)
 
-		self.root.toplevel_dialog_label = ttk.Label(self.root.toplevel_dialog, text='Do you want to enable my parent window again?')
-		self.root.toplevel_dialog_label.pack(side='top')
+		self.root.toplevel_dialog_rad1 = ttk.Radiobutton(self.root.toplevel_dialog,text='\nD&D 5th ed\n', value=GS.UNDEFINED)
+		self.root.toplevel_dialog_rad1.grid(column=0, row=3)
 
-		self.root.toplevel_dialog_yes_button = ttk.Button(self.root.toplevel_dialog, text='Accept', command=self.Close_Toplevel)
-		self.root.toplevel_dialog_yes_button.pack(side='left', fill='x', expand=True)
+		self.root.toplevel_dialog_yes_button = ttk.Button(self.root.toplevel_dialog, text='Accept', command=self.accept_clicked)
+		self.root.toplevel_dialog_yes_button.grid(column=0, row=5)
 
-		self.root.toplevel_dialog_no_button = ttk.Button(self.root.toplevel_dialog, text='Cancel')
-		self.root.toplevel_dialog_no_button.pack(side='right', fill='x', expand=True)
+		self.root.toplevel_dialog_no_button = ttk.Button(self.root.toplevel_dialog, text='Cancel', command=self.cancel_clicked)
+		self.root.toplevel_dialog_no_button.grid(column=1, row=5)
 
-	def Close_Toplevel(self):
+	def accept_clicked(self):
+		self.root.wm_attributes("-disabled", False) # IMPORTANT!
+		self.root.toplevel_dialog.destroy()
+		self.root.deiconify() 
+
+		self.game.set_is_configured_from_string("True")
+
+	def cancel_clicked(self):
 		self.root.wm_attributes("-disabled", False) # IMPORTANT!
 		self.root.toplevel_dialog.destroy()
 		self.root.deiconify() 
